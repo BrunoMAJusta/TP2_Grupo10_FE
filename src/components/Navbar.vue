5654d49072b36601f581e0dfbf75f1365cb8edc0
nunof <nunofag9@hotmail.com>
2020-06-04 09:34:59 +0100

    navbar, first things

---
+++ src/components/Navbar.vue
@@ -0,0 +1,74 @@
+<template>
+    <nav class="navbar navbar-light bg-light">
+        <div class="row" style="width: 100vw">
+            <div class="col-sm-6">
+                <div v-if="this.$store.state.loggedUser.length != 0" style="float: left">
+                    <router-link to="/Home">Início</router-link> |
+                    <router-link to="/table">Informações</router-link>
+                </div>
+                <div v-else style="float: left">
+                    <router-link to="/login">Login</router-link> |
+                    <router-link to="/register">Registo</router-link>
+                </div>
+            </div>
+            <div class="col-sm-6">
+                <form class="form-inline" v-if="this.$store.state.loggedUser.length != 0" style="float: right">
+                    <a class="navbar-brand" style="float: left"> {{this.$store.state.loggedUser.name}}</a>
+                    <button class="btn btn-outline-success" type="button" @click="logout()">Terminar
+                        Sessão</button>
+                </form>
+            </div>
+        </div>
+    </nav>
+</template>
+
+<script>
+    import {
+        mapGetters
+    } from "vuex"
+    export default {
+        data: function () {
+            return {
+                onlineUser: "",
+            }
+        },
+        created() {
+            if (localStorage.getItem("loggedUser")) {
+                this.$store.state.loggedUser = JSON.parse(localStorage.getItem("loggedUser"))
+                this.onlineUser = this.$store.state.loggedUser
+            }
+            this.$store.commit('STORE_ITEMS')
+        },
+        methods: {
+            logout() {
+                alert('Terminou sessão com sucesso!')
+                this.$store.commit('LOGOUT')
+            }
+        },
+        computed: {
+            getName() {
+                if (localStorage.getItem("loggedUser")) {
+                    return this.onlineUser
+                } else {
+                    return "Entrar"
+                }
+            }
+        }
+    }
+</script>
+
+
+<style lang="css" scoped>
+    #nav {
+        padding: 30px;
+    }
+
+    #nav a {
+        font-weight: bold;
+        color: black;
+    }
+
+    #nav a.router-link-exact-active {
+        color: #007bff;
+    }
+</style>
---
+++ src/views/Table.vue
@@ -0,0 +1,51 @@
+<template>
+    <div>
+        <table class="table table-striped table-dark">
+            <thead>
+                <tr>
+                    <th scope="col">Id</th>
+                    <th scope="col">Nome</th>
+                    <th scope="col">Email</th>
+                    <th scope="col">Opções</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr v-for="user of users" :key="user.id">
+                    <th scope="row">{{user.id}}</th>
+                    <td>{{user.name}}</td>
+                    <td>{{user.email}}</td>
+                    <td><button type="button" class="btn btn-primary" @click="edit(user.id)">Editar</button>
+                        <button type="button" class="btn btn-primary" @click="del(user.id)">Eliminar</button>
+                    </td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+</template>
+
+<script>
+    export default {
+        data: () => ({
+            users: []
+        }),
+        created: function () {
+            if (localStorage.getItem("users")) {
+                this.$store.state.users = JSON.parse(localStorage.getItem("users"))
+                this.users = this.$store.state.users
+            }
+            console.log(this.users)
+        },
+        methods: {
+            edit(id) {
+
+            },
+            del(id) {
+
+            }
+        }
+    };
+</script>
+
+<style lang="css" scoped>
+
+</style>
